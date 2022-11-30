fdf4aadfe51fcc13daab3a3fe310e372ba403cab
Restyled.io <commits@restyled.io>
2022-11-30 03:58:31 +0000

    Restyled by prettier

--- src/db_connection.js
+++ src/db_connection.js
@@ -1,15 +1,13 @@
 // Require mysql
-var mysql = require('mysql');
+var mysql = require("mysql");
 // const CLEARDB_DATABASE_URL =
 // 'mysql://b0ca17fbc3e177:7b746377@us-cdbr-east-06.cleardb.net/heroku_2e86a661a20b463?reconnect=true';
 // Create db connection class
-const DATABASE_URL = process.env.DATABASE_URL ||
-                     "mysql://root:mysql@localhost/employee_track_cms";
+const DATABASE_URL =
+  process.env.DATABASE_URL || "mysql://root:mysql@localhost/employee_track_cms";
 
 class DBConnection {
-
   constructor() {
-
     console.log("Connecting to database: " + DATABASE_URL);
     this.url = DATABASE_URL;
     this.connection = mysql.createPool(this.url);
@@ -18,7 +16,7 @@ class DBConnection {
     // this.connection.connect(function (err) {
     //     if (err) throw err;
     // });
-  };
+  }
 
   static getInstance() {
     if (!DBConnection.instance) {
